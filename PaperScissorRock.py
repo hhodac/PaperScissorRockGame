1f48851d13bdab3ab126f1b00aa91d27d0a5ae06
Hai HoDac <hhodac@student.unimelb.edu.au>
2020-02-26 20:46:12 +1100

    Added MySQL database connection

--- PaperScissorRock.py
+++ PaperScissorRock.py
@@ -1,13 +1,32 @@
 import pygame
 import random
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    mydb = mysql.connector.connect( host = "localhost",
+                                    database = "PaperScissorRock",
+                                    user = "root",
+                                    password = "password",
+                                    auth_plugin = "mysql_native_password")
+    if mydb.is_connected():
+        print('Database successfully connected!')
+        cursor = mydb.cursor()
+except Error as e:
+    print('Error while connecting to MySQL', e)
 
 pygame.init()
 
 display_width = 400
 display_height = 200
 
+black = (0, 0 , 0)
 white = (255, 255, 255)
 blue = (0, 0, 128)
+red = (200, 0, 0)
+green = (0, 200, 0)
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Paper - Scissor - Rock')
@@ -48,6 +67,7 @@ def winner_display(player):
     textRect = textDisplay.get_rect()
     textRect.center = (display_width // 2, display_height * 0.9)
     gameDisplay.blit(textDisplay, textRect)
+    return text
 
 def playerA_action():
     actionChoice = random.choice([1, 2, 3])
@@ -76,34 +96,87 @@ def whoWins(actionA, actionB):
         # print('Draw')
         return None
 
-crashed = False
-actionA = playerA_action()
-actionB = playerB_action()
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
 
-while not crashed:
+def button(msg, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    # print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac, (x, y, w, h))
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(gameDisplay, ic, (x, y, w, h))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        # print(event)
+    smallText = pygame.font.Font("freesansbold.ttf", 10)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
 
-        ###
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                actionA = playerA_action()
-                actionB = playerB_action()
-                print(', '.join([actionA, actionB]))
-        ###
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                game_quit()
+
+        gameDisplay.fill(white)
+        normalText = pygame.font.Font('freesansbold.ttf', 30)
+        TextSurf, TextRect = text_objects("Paper - Scissor - Rock", normalText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+
+        button("GO!", 50, 150, 100, 40, bright_green, green, game_loop)
+        button("QUIT!", 250, 150, 100, 40, bright_red, red, game_quit)
+
+        pygame.display.update()
+        clock.tick(15)
+
+def game_loop():
+    crashed = False
+    actionA = playerA_action()
+    actionB = playerB_action()
+
+    while not crashed:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+            # print(event)
+
+            ###
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    actionA = playerA_action()
+                    actionB = playerB_action()
+                    print(', '.join([actionA, actionB]))
+                    cursor.execute("INSERT INTO GameRecords (P1Action, P2Action, Results) "
+                                   "VALUES (%s, %s, %s);",
+                                   (actionA, actionB, winner_display(whoWins(actionA, actionB))))
+                    mydb.commit()
+                    print("Record inserted successfully into GameRecords table.")
+            ###
 
 
-    gameDisplay.fill(white)
-    playerA_display(actionA)
-    playerB_display(actionB)
-    winner_display(whoWins(actionA, actionB))
+        gameDisplay.fill(white)
+        playerA_display(actionA)
+        playerB_display(actionB)
+        winner_display(whoWins(actionA, actionB))
 
 
-    pygame.display.update()
-    clock.tick(30)
+        pygame.display.update()
+        clock.tick(30)
 
-pygame.quit()
-quit()
+def game_quit():
+    pygame.quit()
+    quit()
+
+game_intro()
+if mydb.is_connected():
+    mydb.close()
